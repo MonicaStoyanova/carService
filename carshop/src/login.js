ff172018f2835af629f4c56405645814fc518ca2
Monica Stoyanova <114282401+MonicaStoyanova@users.noreply.github.com>
2023-03-08 20:35:11 +0200

    Add files via upload

---
+++ carshop/src/login.js
@@ -0,0 +1,40 @@
+import { showHome } from "./home.js";
+
+const loginSection = document.getElementById("login-link");
+
+const loginForm = loginSection.querySelector("#login-form");
+loginForm.addEventListener("submit", onLogin);
+
+export function showLogin() {
+  document.querySelector("main").replaceChildren(loginSection);
+}
+
+async function onLogin(event) {
+  event.preventDefault();
+  const formData = new FormData(loginForm);
+  const { email, password } = Object.fromEntries(formData.entries());
+
+  try {
+    const response = await fetch("http://localhost:3030/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    if (response.ok == false) {
+      const error = await response.json();
+      throw error;
+    }
+    const userData = await response.json();
+
+    localStorage.setItem("email", userData.email);
+    localStorage.setItem("id", userData._id);
+    localStorage.setItem("accessToken", userData.accessToken);
+
+    loginForm.reset();
+    showHome();
+  } catch (error) {
+    alert(error.message);
+  }
+}
